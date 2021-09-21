c19e9c0a0654ff9bd1bbed1bc51dd21e35e85d60
Lee Shiueh <lee.shiueh@gmail.com>
2021-09-21 14:48:47 +0800

    Add uoffice main function implementation

--- uoffice/apis/__init__.py
+++ uoffice/apis/__init__.py
@@ -1,2 +1,3 @@
-from .cron_handlers import NotifyDutyOwnerCronHandler
+from .cron_handlers import NotifyUofCleanDutyHandler
+from .cron_handlers import ListEnvVarsHandler
 from .echo_handler import EchoHandler
--- uoffice/urls.py
+++ uoffice/urls.py
@@ -2,7 +2,8 @@
 import logging
 from django.conf.urls import url
 from django.views.decorators.csrf import csrf_exempt
-from uoffice.apis import NotifyDutyOwnerCronHandler
+from uoffice.apis import NotifyUofCleanDutyHandler
+from uoffice.apis import ListEnvVarsHandler
 from uoffice.apis import EchoHandler
 
 
@@ -12,8 +13,10 @@ urlpatterns = [
     # path('admin/', admin.site.urls),
 
     # request handlers
-    url(r'^notify_duty_owner$', csrf_exempt(NotifyDutyOwnerCronHandler.as_view()),
-        name=NotifyDutyOwnerCronHandler.ID),
+    url(r'^notify_uof_clean_duty$', csrf_exempt(NotifyUofCleanDutyHandler.as_view()),
+        name=NotifyUofCleanDutyHandler.ID),
+    url(r'list_env_vars', csrf_exempt(ListEnvVarsHandler.as_view()),
+        name=ListEnvVarsHandler.ID),
     url(r'^echo$', csrf_exempt(EchoHandler.as_view()), name=EchoHandler.ID),
     url(r'^$', csrf_exempt(EchoHandler.as_view()))
 ]
