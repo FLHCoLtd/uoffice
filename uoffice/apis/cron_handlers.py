64b2306b24e3805d1bbc37a3ee6f293ad265fc79
Lee Shiueh <lee.shiueh@gmail.com>
2021-09-21 16:24:31 +0800

    Fix default page handler bugs

--- uoffice/apis/cron_handlers.py
+++ uoffice/apis/cron_handlers.py
@@ -4,7 +4,7 @@ import json
 import uuid
 import logging
 import googleapiclient.discovery
-import dotenv
+# import dotenv
 import requests
 from datetime import date
 from django.views.generic import View
@@ -12,20 +12,22 @@ from django.http import JsonResponse
 from google.oauth2 import service_account
 
 logger = logging.getLogger()
-dotenv.load_dotenv()
-gcp_account_info = os.getenv('GCP_SERVICE_ACCOUNT_INFO')
-sheet_url = os.getenv('SPREADSHEET_URL')
-sheet_id = os.getenv('SPREADSHEET_ID')
-sheet_range = os.getenv('SPREADSHEET_RANGE_NAME')
-line_token = os.getenv('LINE_NOTIFY_TOKEN')
-line_api_url = os.getenv('LINE_NOTIFY_API_URL')
+# dotenv.load_dotenv()
 
 
 def get_today_on_duty_name_n_team():
     """Query goodle spreadsheet for on duty name/team of the day"""
     # print(f'gcp_account_info {json.loads(gcp_account_info)}')
+    gcp_account_info = os.getenv('GCP_SERVICE_ACCOUNT_INFO')
+    sheet_id = os.getenv('SPREADSHEET_ID')
+    sheet_range = os.getenv('SPREADSHEET_RANGE_NAME')
     if not gcp_account_info:
         raise ValueError('No GCP_SERVICE_ACCOUNT_INFO')
+
+    if os.path.isfile(gcp_account_info):
+        with open(gcp_account_info) as fh:
+            gcp_account_info = fh.read()
+
     credentials = service_account.Credentials.from_service_account_info(
         json.loads(gcp_account_info)
     )
@@ -35,12 +37,17 @@ def get_today_on_duty_name_n_team():
         spreadsheetId=sheet_id,
         range=sheet_range)
     response = request.execute()
-    return response.get('values')[0][0]
+    values = response.get('values', None)
+    if values:
+        return values[0][0]
+    else:
+        return None
 
 
 class NotifyUofCleanDutyHandler(View):
     """Query duty owner name from google sheet and push notification to line group"""
     ID = f'{uuid.uuid4()}'
+    sheet_url = os.getenv('SPREADSHEET_URL')
 
     def get(self, _):
         duty = get_today_on_duty_name_n_team()
@@ -55,6 +62,8 @@ class NotifyUofCleanDutyHandler(View):
         return JsonResponse({'notification': f'{msg}'})
 
     def send_line_push_msg(self, msg):
+        line_token = os.getenv('LINE_NOTIFY_TOKEN')
+        line_api_url = os.getenv('LINE_NOTIFY_API_URL')
         payload = {'message': msg}
         headers = {'Authorization': f'Bearer {line_token}'}
         logger.debug(f'send_line_push_msg payload {payload}')
